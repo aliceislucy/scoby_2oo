e91934ea2894a5d47e5e67f3b382710c8ad3f9e6
jturqueti <julia.turqueti@gmail.com>
2021-06-02 15:27:06 +0200

    yoyo

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./components/Auth/AuthProvider";
 import App from "./App";
-
 import "./styles/reset.css";
 import "./styles/global.css";
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,74 @@
 import React from "react";
+import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "../styles/Home.css";
+import axios from "axios";
 
-const Home = (props) => {
-  // Implement react map box here.
-  return (
-    <div>
-      <h1>MAPBOX MAP HERE</h1>
-      <p>On home /</p>
-    </div>
-  );
-};
+const Map = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+});
 
-export default Home;
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: [],
+    };
+  }
+
+  emoji = (itemCategory) => {
+    console.log(itemCategory)
+    if (itemCategory === "Plant") {
+      return "https://www.iconpacks.net/icons/2/free-plant-icon-1573-thumb.png";
+    } else if (itemCategory === "Kombucha") {
+      return "https://img.icons8.com/plasticine/2x/26e07f/mate.png";
+    } else if (itemCategory === "Kefir") {
+      return "https://image.pngaaa.com/872/610872-middle.png";
+    } else {
+      return "https://www.pngrepo.com/download/167926/rabbit.png";
+    }
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:2021/api/item")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          item: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log(this.state.item);
+    return (
+      <div>
+        <h1>MAPBOX MAP HERE</h1>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "100vh",
+            width: "100vw",
+          }}
+          center={[2.333333, 48.866667]}
+        >
+          {this.state.item.map((item) => {
+            return (
+              <Marker coordinates={item.location.coordinates} anchor="bottom">
+                <img className="icon2" alt="" src={this.emoji(item.category[0])} />
+              </Marker>
+            );
+          })}
+        </Map>
+        ;
+      </div>
+    );
+  }
+}
+
+//Kombucha", "Kefir", "Vinegar
